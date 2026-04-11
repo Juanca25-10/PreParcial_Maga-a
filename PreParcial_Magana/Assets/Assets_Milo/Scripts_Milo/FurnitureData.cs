9f9dc27c56fef7637cd92283512730eb3cfddbf5
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2026-04-10 23:44:00 -0500

    Edicion Presupuesto
    gg

---
+++ PreParcial_Magana/Assets/Assets_Milo/Scripts_Milo/FurnitureData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class FurnitureData : MonoBehaviour
+{
+    public string idMueble; // Ejemplo: "sofa_cuero_01"
+    public string nombreMueble;
+    public float precio;
+}
--- PreParcial_Magana/Assets/Assets_Waen/Scritps/FurniturePlacer.cs
+++ PreParcial_Magana/Assets/Assets_Waen/Scritps/FurniturePlacer.cs
@@ -5,10 +5,18 @@ public class FurniturePlacer : MonoBehaviour
     [Header("Prefab del mueble")]
     [SerializeField] private GameObject mueblePrefab;
 
-    // Este método se conecta al OnPlaneSelected del PlaneSelectionManager
     public void ColocarMueble(Pose pose)
     {
-        Instantiate(mueblePrefab, pose.position, pose.rotation);
-        Debug.Log("Mueble colocado en: " + pose.position);
+        FurnitureData info = mueblePrefab.GetComponent<FurnitureData>();
+
+        if (info != null && BudgetManager.Instance.PuedeComprar(info.precio))
+        {
+            GameObject nuevo = Instantiate(mueblePrefab, pose.position, pose.rotation);
+            BudgetManager.Instance.RegistrarCompra(nuevo, info.precio);
+        }
+        else
+        {
+            Debug.LogWarning("¡No tienes plata o el prefab no tiene FurnitureData!");
+        }
     }
 }
